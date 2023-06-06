f7d2bb5f1f820e5bf68692a8ec1a550adb4cc216
Faraz Davachi <135701315+FarazDavachi@users.noreply.github.com>
2023-06-06 13:43:36 +0330

    Add files via upload

---
+++ sidebar/script.js
@@ -0,0 +1,5 @@
+const selectElement = (element) => document.querySelector(element);
+const header = selectElement('header');
+const mainContent = selectElement('main');
+selectElement('.hamburger').addEventListener('click', () => { header.classList.toggle('active') });
+mainContent.classList.toggle('active');
